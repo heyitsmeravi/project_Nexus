072ac68bbe3ddbcc1bcd64f5cd9f3626b1a04307
heyitsmeravi <ravijnv2428@gmail.com>
2026-03-07 11:09:24 +0530

    chore: Navbar component  added with tailwind styling update

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,7 +1,15 @@
 export default function Navbar(){
     return (
-        <>
-            <h1 className="" > Project Nexus</h1>
-        </>
+        <nav className="bg-gray-200 px-6 py-4 flex justify-between items-center">
+            <h1 className="text-3xl text-blue-950 font-bold ">Project Nexus</h1>
+            <div className="flex gap-10">
+                <ul className="flex gap-10 text-blue-950 text-lg font-bold items-center ">
+                    <li className="cursor-pointer hover:text-blue-400 hover:underline">Home</li>
+                    <li className="cursor-pointer hover:text-blue-400 hover:underline">Upload Resume</li>
+                    <li className="cursor-pointer hover:text-blue-400 hover:underline">Dashboard</li>
+                </ul>
+                <button className=" bg-white px-2 py-1 rounded-md hover:bg-gray-100 cursor-pointer" type='button'>Logout</button>
+             </div>
+        </nav>
     )
 }
