6bce6e76124ed590b7ee6378ba7c216f511e142e
heyitsmeravi <ravijnv2428@gmail.com>
2026-03-08 18:48:13 +0530

    chore: update Navbar with Responsiveness

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,16 +1,48 @@
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
+import { useState,useEffect } from "react";
+import {Menu, X} from "lucide-react";
 export default function Navbar(){
+    const navLinkClass=({isActive}:{isActive:boolean})=>isActive? "text-blue-600 underline text-2l cursor-default":"text-blue-950 hover:text-blue-400 hover:underline cursor-pointer transition-colors";
+    const [scrolled, setScrolled] = useState(false);
+    const [menuOpen,setMenuOpen] = useState(false);
+    useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      setScrolled(scrollY > 50);
+    };
+    
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
     return (
-        <nav className="bg-gray-200 px-6 py-4 flex justify-between items-center">
-            <h1 className="text-3xl text-blue-950 font-bold ">Project Nexus</h1>
-            <div className="flex gap-10">
-                <ul className="flex gap-10 text-blue-950 text-lg font-bold items-center ">
-                    <li><NavLink className="cursor-pointer hover:text-blue-400 hover:underline" to="/">Home</NavLink></li>
-                    <li><NavLink className="cursor-pointer hover:text-blue-400 hover:underline" to="/upload">Upload Resume</NavLink></li>
-                    <li><NavLink className="cursor-pointer hover:text-blue-400 hover:underline" to="/dashboard">Dashboard</NavLink></li>
+        <nav className={`fixed top-0 left-0 w-full z-50 bg-gray-200/40 backdrop-blur-md px-6 flex justify-between items-center ${scrolled ? 'bg-black/40 py-2 backdrop-blur-md shadow-2xl' : 'py-4'}`} >
+            <h1 className={`text-blue-950 ${scrolled?'text-7xl':'text-2xl'}`}>Project Nexus</h1>
+            <div className="hidden md:flex gap-10 items-center">
+                <ul className="flex gap-10 text-lg font-bold">
+                    <li><NavLink className={navLinkClass} to="/">Home</NavLink></li>
+                    <li><NavLink className={navLinkClass} to="/upload">Upload Resume</NavLink></li>
+                    <li><NavLink className={navLinkClass} to="/dashboard">Dashboard</NavLink></li>
                 </ul>
                 <button className=" bg-white px-2 py-1 rounded-md hover:bg-gray-100 cursor-pointer" type='button'>Logout</button>
              </div>
+            <button
+              className="md:hidden text-2xl"
+              onClick={() => setMenuOpen(!menuOpen)}
+            >
+              {menuOpen ? <X size={28}></X> : <Menu size={28}/>}
+            </button>
+            {
+              menuOpen&& (
+                <div className="absolute top-full left-0 w-full bg-white shadow-md flex flex-col items-center gap-6 py-6 md:hidden">
+                  <NavLink className={navLinkClass} to="/" onClick={()=>setMenuOpen(false)}>Home</NavLink>
+                  <NavLink className={navLinkClass} to="/upload" onClick={()=>setMenuOpen(false)}>Upload Resume</NavLink>
+                  <NavLink className={navLinkClass} to="/dashboard" onClick={()=>setMenuOpen(false)}>Dashboard</NavLink>
+                  <button className="bg-gray-100 px-4 py-2 rounded-md" type='button'>Logout</button>
+                </div>
+              )
+            }
         </nav>
     )
 }
